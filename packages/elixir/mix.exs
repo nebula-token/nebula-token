e0f56cdbdd765d325cf8a4a805fa1f194c209fe8
matteo.teodori <matteo.teodori@dynacode.it>
2026-07-30 21:45:56 +0200

    chore(release): 1.0.1-rc.2

--- packages/elixir/mix.exs
+++ packages/elixir/mix.exs
@@ -29,7 +29,7 @@ defmodule NebulaToken.MixProject do
   def project do
     [
       app: :nebula_token,
-      version: "1.0.1-rc.1",
+      version: "1.0.1-rc.2",
       elixir: "~> 1.18",
       description:
         "Opaque rotating refresh tokens (RFC 9700 model): rotation, reuse detection, family revocation, sender binding.",
